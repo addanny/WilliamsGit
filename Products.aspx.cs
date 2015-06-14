a69585948eca8f01f0d06a16342898ba9335145c
brad busch <bbuschrn@gmail.com>
2015-06-14 09:39:52 -0400

    weird stuff broke - refixed

--- Products.aspx.cs
+++ Products.aspx.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/**
+*Senior Project
+*Group B
+*May Session 2015
+*CIS470
+*Brad Busch, David Dalloo, Ionut Sava, Christina Roark, Jose Morales
+ **/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -15,7 +23,7 @@ public partial class Products : Page
     protected void Page_Load(object sender, EventArgs e)
     {
         Label1.Visible = false;
-        txtPlaqueQty.Visible = false;
+        txtTShirtQty.Visible = false;
         Label2.Visible = false;
         txtPlaqueQty.Visible = false;
         linkTShirtAdd.Visible = false;
@@ -25,21 +33,19 @@ public partial class Products : Page
     //im going to assume that this is the "Buy" button for the plaques. this has since been renamed
     protected void Button1_Click(object sender, EventArgs e)
     {
-        Label1.Visible = true;
+        Label2.Visible = true;
         txtPlaqueQty.Visible = true;
         txtPlaqueQty.Focus();
-        linkTShirtAdd.Visible = true;
-        lblTShirtWarning.Visible = false;
+        linkPlaqueAdd.Visible = true;
     }
 
     //im going to assume that this is the "Buy" button for the tshirts. this has since been renamed
     protected void Button2_Click(object sender, EventArgs e)
     {
-        Label2.Visible = true;
-        txtPlaqueQty.Visible = true;
-        txtPlaqueQty.Focus();
-        linkPlaqueAdd.Visible = true;
-        lblPlaqueWarning.Visible = false;
+        Label1.Visible = true;
+        txtTShirtQty.Visible = true;
+        txtTShirtQty.Focus();
+        linkTShirtAdd.Visible = true;
     }
 
     //i am unsure what these two functions are being used for. will leave them be for now
@@ -53,8 +59,6 @@ public partial class Products : Page
 
     }
 
-    //will add the qty of the plaques to the shop page. will catch non-integers
-
     //will add the qty of the tshirts to the shop page. will catch non-integers
     protected void linkTShirtAdd_Click(object sender, EventArgs e)
     {
@@ -62,11 +66,11 @@ public partial class Products : Page
         lblTShirtWarning.Visible = false;
 
         //determine that the value is parsable - if it is, assign values. Else, display error
-        if (int.TryParse(txtPlaqueQty.Text, out intOutput))
+        if (int.TryParse(txtTShirtQty.Text, out intOutput))
         {
             ProductClass.productName = "T-Shirt";
             ProductClass.productPrice = 10;
-            ProductClass.productQty = Int32.Parse(txtPlaqueQty.Text);
+            ProductClass.productQty = Int32.Parse(txtTShirtQty.Text);
             //price of tshirts
             int totalTShirtPrice = ProductClass.productPrice * ProductClass.productQty;
             //display summary of the order
@@ -80,7 +84,7 @@ public partial class Products : Page
         {
             lblTShirtWarning.Visible = true;
             lblTShirtWarning.Text = "Please enter a valid number";
-            txtPlaqueQty.Text = "";
+            txtTShirtQty.Text = "";
         }
     }
 
@@ -97,7 +101,7 @@ public partial class Products : Page
         lblPlaqueWarning.Visible = false;
 
         //determine that the value is parsable - if it is, assign values. Else, display error
-        if (int.TryParse(txtPlaqueQty.Text, out intOutput))
+        if (int.TryParse(txtTShirtQty.Text, out intOutput))
         {
             ProductClass.productName = "Plaque";
             ProductClass.productPrice = 10;
@@ -115,7 +119,7 @@ public partial class Products : Page
         {
             lblPlaqueWarning.Visible = true;
             lblPlaqueWarning.Text = "Please enter a valid number";
-            txtPlaqueQty.Text = "";
+            txtTShirtQty.Text = "";
         }
     }
 }
--- Shop.aspx.cs
+++ Shop.aspx.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/**
+*Senior Project
+*Group B
+*May Session 2015
+*CIS470
+*Brad Busch, David Dalloo, Ionut Sava, Christina Roark, Jose Morales
+ **/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
