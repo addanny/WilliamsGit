82898179b6e3ab4d2616b54573c06467d7a5124c
brad busch <bbuschrn@gmail.com>
2015-06-14 11:52:09 -0400

    Login authenticated. Works. Will do more with it later - have to work tonight

--- Login.aspx.cs
+++ Login.aspx.cs
@@ -8,10 +8,13 @@
 
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+using System.Web.Configuration;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Windows.Forms;
 
 public partial class _Default : System.Web.UI.Page
 {
@@ -22,6 +25,35 @@ public partial class _Default : System.Web.UI.Page
 
     protected void Login1_Authenticate(object sender, AuthenticateEventArgs e)
     {
+        //declare the variables
+        string username = Login1.UserName;
+        string pwd = Login1.Password;
+        string sqlUserName;
+        string s = WebConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+        SqlConnection con = new SqlConnection(s);
 
+        //open the connection
+        con.Open();
+        
+        //insert SQL statement
+        sqlUserName = "SELECT Username,Password FROM tblLogin WHERE Username ='" + username + "' AND Password ='" + pwd + "'";
+        SqlCommand cmd = new SqlCommand(sqlUserName, con);
+        string CurrentName = (string)cmd.ExecuteScalar();
+
+        //authenticate user
+        if (CurrentName != null)
+        {
+            //access granted
+            Session["UserAuthentication"] = username;
+            Session.Timeout = 1;
+            MessageBox.Show(new Form { TopMost = true }, "Successful");
+            Response.Redirect("~/Home.aspx");
+        }
+        else
+        {
+            //access denied
+            Session["UserAuthentication"] = "";
+        }
     }
+
 }
